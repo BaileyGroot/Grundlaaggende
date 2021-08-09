573bcc7fc3253bee422e3dc72f59327687f0a56f
Grootrix <grootrix@gmail.com>
2021-08-09 10:30:32 +0200

    Add project files.

---
+++ CGO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            int a = 12;
+
+            double b = 5.1;
+
+
+
+            // Visuel Studio vil ikke oversætte denne kode begrunde af de to forskellige variabler der bliver brugt!
+
+            // Int: Kan kun tage 1-2-3 (Hele tal)
+
+            // Double: Kan kun (Tage både tage 1 og 1.1)
+
+
+
+            int c = a + b;
+
+
+
+            Console.WriteLine(c);
+
+            
+           //Opgave C
+
+           // Variablen (Double) kan både tage 1 (Helt tal) og 1.5 (comma tal/halvt tal)
+
+
+
+            double d = a + b;
+
+
+
+            Console.WriteLine(d);
+
+
+            // Key Reader
+            Console.ReadKey();
+        }
+    }
+}
